745556b19aa02fa19bf381b616d2fe9fc09f5cf0
Gabriel <mail@gabrieldavison.me.uk>
2020-10-16 17:16:17 +0100

    Pre eject commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
-import React from "react"
+import React, {useEffect, useCallback} from "react"
 import "./App.css"
+import * as WaveSurfer from "../node_modules/wavesurfer.js/dist/wavesurfer"
+import RegionsPlugin from '../node_modules/wavesurfer.js/dist/plugin/wavesurfer.regions'
+import sample from './samples/'
 
-const App: React.FC = () => <div> Wave Sampler </div>
+const wavesurfer:any = WaveSurfer.create({
+	container: "#wavesurfer-container",
+	waveColor: "violet",
+	progressColor: "purple",
+	backend: "MediaElementWebAudio",
+	plugins: [
+		RegionsPlugin.create({
+			regionsMinLength: 1,
+			regions: [],
+			dragSelection: {
+				slop: 1,
+			},
+		}),
+	],
+})
 
+
+const App: React.FC = () => {    
+    const loadSample = useCallback((sample:any) => {
+        wavesurfer.load(sample)
+    }, [])
+
+    useEffect(() => loadSample(sample))
+    return (<div> Wave Sampler </div>)
+}
 export default App
