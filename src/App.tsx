e653f2df81342ad45be05210e4f361864e3b2ac1
jaqarrick <pigmaask@gmail.com>
2020-10-20 19:05:39 -0700

    updated functionality of App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import React, { useEffect, useState, useRef, useCallback } from "react"
+import React, { useEffect, useState, useRef, useCallback, useMemo } from "react"
 import "./App.css"
 import * as WaveSurfer from "../node_modules/wavesurfer.js/dist/wavesurfer"
 import RegionsPlugin from "../node_modules/wavesurfer.js/dist/plugin/wavesurfer.regions"
 //@ts-ignore
 // import sample from "./garoto.mp3"
 import Controls from "./components/controls/Controls"
+import { SampleTimesObject } from "./types"
+import { Hooks } from "html-webpack-plugin"
 
 const App: React.FC = () => {
 	const waveFormRef = useRef<any>(null)
 	const wavesurfer = useRef<any>(null)
 	const [mediaRecorder, setMediaRecorder] = useState<null | MediaRecorder>(null)
 
+	//Initial App Render: setup wavesurfer object and connect ac backend to Media Recorder
 	useEffect(() => {
 		wavesurfer.current = WaveSurfer.create({
 			container: waveFormRef.current,
@@ -19,7 +22,7 @@ const App: React.FC = () => {
 			backend: "MediaElementWebAudio",
 			plugins: [
 				RegionsPlugin.create({
-					regionsMinLength: 1,
+					regionsMinLength: 0.0001,
 					maxRegions: 1,
 					regions: [],
 					dragSelection: {
@@ -29,28 +32,60 @@ const App: React.FC = () => {
 			],
 		})
 		// Creates a mediastream for mediaRecorder
-		const streamDestination = wavesurfer.current.backend.ac.createMediaStreamDestination()
-
+		const streamDestination: MediaStreamAudioDestinationNode = wavesurfer.current.backend.ac.createMediaStreamDestination()
+		wavesurfer.current.load("./NOISE.wav")
 		// Creates a gainNode to use as a wavesurfer filter (just needs to be something for the audio to pass through)
-		const gainNode = wavesurfer.current.backend.ac.createGain()
+		const gainNode: GainNode = wavesurfer.current.backend.ac.createGain()
 		// Connects gain node to the audio stream
 		gainNode.connect(streamDestination)
 		wavesurfer.current.backend.setFilter(gainNode)
 		setMediaRecorder(new MediaRecorder(streamDestination.stream))
-		wavesurfer.current.load("./NOISE.wav")
-		wavesurfer.current.on("ready", () => wavesurfer.current.play())
 	}, [])
 
+	const [sampleTimes, setSampleTimes] = useState<SampleTimesObject>({
+		start: 0,
+		end: 0,
+	})
+
+	const sampleDuration = useMemo(
+		(): number => sampleTimes.end - sampleTimes.start,
+		[sampleTimes]
+	)
 	const playSelectedAudio = useCallback(() => {
 		// Look at this later and see if theres a better solution
-		// let start
-		// let end
-		// for (let id in wavesurfer.current.regions.list) {
-		// 	start = wavesurfer.current.regions.list[id].start
-		// 	end = wavesurfer.current.regions.list[id].end
-		// }
-		// wavesurfer.current.play(start, end)
-	}, [])
+		const { start, end } = sampleTimes
+		wavesurfer.current.play(start, end)
+	}, [sampleTimes])
+
+	const updateSampleDuration = useCallback(() => {
+		let start
+		let end
+		for (let id in wavesurfer.current.regions.list) {
+			start = wavesurfer.current.regions.list[id].start
+			end = wavesurfer.current.regions.list[id].end
+			setSampleTimes({
+				start,
+				end,
+			})
+		}
+	}, [setSampleTimes])
+	//whenever a region is updated, update the start and end times of the region
+	useEffect(() => {
+		wavesurfer.current.on("region-updated", () => {
+			updateSampleDuration()
+			// console.log(wavesurfer.current.regions.list)
+		})
+	})
+
+	const startRecording = useCallback(() => {
+		console.log("recording started")
+		playSelectedAudio()
+		console.log(sampleDuration)
+		if (mediaRecorder) {
+			mediaRecorder.start()
+			setTimeout(() => mediaRecorder.stop(), sampleDuration * 1000)
+		}
+	}, [sampleDuration, playSelectedAudio, mediaRecorder])
 
 	const stopSelectedAudio = useCallback(() => {
 		if (wavesurfer.current) {
@@ -64,6 +99,7 @@ const App: React.FC = () => {
 			<Controls
 				playSelectedAudio={playSelectedAudio}
 				stopSelectedAudio={stopSelectedAudio}
+				startRecording={startRecording}
 			/>
 		</>
 	)
---
+++ src/types.ts
@@ -0,0 +1,4 @@
+export interface SampleTimesObject {
+	start: number
+	end: number
+}
